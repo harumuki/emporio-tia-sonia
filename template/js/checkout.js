3f8ec2d2050458a7bfdc7c859eb1743e284ab4ab
Matheus <matheus@e-com.club>
2020-08-26 13:56:03 -0300

    chore: update acompanhar pedido

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,13 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+import ecomCart from '@ecomplus/shopping-cart'
+const conta = document.getElementById('account') || false
+const pedido = document.getElementById('order') || false
+const acompanhar = document.getElementById('containerAcompanhar')
+ecomCart.on('change', ({ data }) => {
+  if (conta.length || pedido.length) {
+    acompanhar.style.display = 'block'
+  } else {
+    acompanhar.style.display = 'none'
+  }
+})
